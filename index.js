4542833f01205a3387136cbd1b26ca0aa911f2f7
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-03-30 16:19:44 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+introduction = (name) => `Hi, my name is ${name}.`
+
+introductionWithLanguage = (name, language) => `Hi, my name is ${name} and I am learning to program in ${language}.`
+
+introductionWithLanguageOptional = (name, language = "JavaScript") => `Hi, my name is ${name} and I am learning to program in ${language}.`
